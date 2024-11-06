f5bcb5cd511447bd2b19165d19ee7a13a77caaeb
Thierry Boiago <thierry123123@gmail.com>
2024-11-05 22:00:32 -0300

    core: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,7 @@
+const Subscription = () => {
+    return (
+        <h1>Subscription</h1>
+    )
+}
+
+export default Subscription
---
+++ app/transactions/page.tsx
@@ -0,0 +1,7 @@
+const Transactions = () => {
+    return (
+        <h1>Transactions page</h1>
+    )
+}
+
+export default Transactions
